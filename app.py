34b5f7db609466e63a19de822368e385b5f0dc24
rainboowbee <rainboowbee@gmail.com>
2024-04-27 22:47:11 +0300

    add project

---
+++ app.py
@@ -0,0 +1,171 @@
+from flet import *
+import parser
+
+path = ''
+save_path = ''
+def main(page: Page):
+    page.title = 'XMLparser'
+
+    def parse_xml(e):
+        global path
+        players_df, tournament_df = parser.parse_xml_to_dataframe(path)
+
+        players_rows = []
+        for _, row in players_df.iterrows():
+            cells = [
+                DataCell(Text(str(row['№']))),
+                DataCell(Text(row['ФИО'])),
+                DataCell(Text(row['Город'])),
+                DataCell(Text(row['Год рождения'])),
+                DataCell(Text(row['рейтинг'])),
+            ]
+            players_rows.append(DataRow(cells=cells))
+        
+        data_players = DataTable(
+            data_row_max_height=35,
+            column_spacing=10,
+            columns=[
+                DataColumn(Text("№")),
+                DataColumn(Text("ФИО")),
+                DataColumn(Text("Город")),
+                DataColumn(Text("Год рождения")),
+                DataColumn(Text("рейтинг")),
+            ],
+            rows=players_rows,
+        )
+
+        rows_tournament = []
+        for _, row in tournament_df.iterrows():
+            for col in tournament_df.columns:
+                cells = [
+                    DataCell(Text(col)),
+                    DataCell(Text(row[col]))
+                ]
+                rows_tournament.append(DataRow(cells=cells))
+        
+        tournament_columns = [
+            DataColumn(Text("Info")),
+            DataColumn(Text("Value"))
+        ]
+        
+        tournament_data = DataTable(
+            columns=tournament_columns,
+            rows=rows_tournament
+        )
+        lv_players.controls.clear()
+        lv_tournament.controls.clear()
+
+        lv_players.controls.append(data_players)
+        lv_tournament.controls.append(tournament_data)
+
+        page.update()
+
+    
+    lv_players = ListView(expand=1)
+    lv_tournament = ListView(expand=1)
+
+
+    # Pick files dialog
+    def pick_files_result(e: FilePickerResultEvent):
+        global path
+        path = ''
+        if not e.files:
+            selected_files.value = 'Файл не выбран'
+        else:
+            for el in e.files:
+                selected_files.value = el.path
+        path = selected_files.value
+        parse_xml(path)
+
+    pick_files_dialog = FilePicker(on_result=pick_files_result)
+    selected_files = Text()
+
+    # Save file dialog
+    def save_file_result(e: FilePickerResultEvent):
+        global save_path
+        save_path = ''
+        save_file_path.value = e.path + '.xlsx' if e.path else "Cancelled!"
+
+        save_path = save_file_path.value
+        parser.parse_xml_and_save_to_excel(path, save_path)
+        print('Успешно сохранено в ' + save_path)
+
+
+    save_file_dialog = FilePicker(on_result=save_file_result)
+    save_file_path = Text()
+
+
+    # hide all dialogs in overlay
+    page.overlay.extend([pick_files_dialog, save_file_dialog])
+
+
+    page.add(
+        Column(
+            [
+                Row(
+                    [
+                        ElevatedButton(
+                            "Pick files",
+                            icon=icons.UPLOAD_FILE,
+                            on_click=lambda _: pick_files_dialog.pick_files(
+                                allow_multiple=True
+                                ),
+                        ),
+                        ElevatedButton(
+                            "Save file",
+                            icon=icons.SAVE,
+                            on_click=lambda _: save_file_dialog.save_file(),
+                        ),
+                    ]
+                )
+            ],
+            adaptive=True
+        ),
+        Row(
+            [
+                Column(
+                    [
+                        Container(
+                            content=lv_players,
+                            width=700,
+                            height=500,
+                            border=border.all(2, colors.BLACK),
+                        ),
+                    ],
+                ),
+                Column(
+                    [
+                        Container(
+                            content=lv_tournament,
+                            width=700,
+                            height=500,
+                            border=border.all(2, colors.BLACK),
+                        )
+                    ],
+                    adaptive=True
+                )
+            ],
+            adaptive=True,
+            alignment=MainAxisAlignment.CENTER
+        ),
+        Column(
+            [
+                Row(
+                    [
+                        IconButton(icon=icons.COPYRIGHT, url='https://github.com/rainboowbee'),
+                        Text(
+                            value='Made by Dmitry Glumov',
+
+                        ),
+                    ],
+                    alignment=MainAxisAlignment.CENTER,
+                    adaptive=True
+                )
+            ],
+            alignment=MainAxisAlignment.END,
+            adaptive=True
+        )
+    )
+
+app(target=main)
+
---
+++ example.py
@@ -0,0 +1,117 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+def parse_xml_to_dataframe(xml_file_path):
+    # Загружаем и разбираем XML файл
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
+    
+    # Парсим информацию о игроках
+    players_data = []
+    for idx, player in enumerate(root.findall('.//Player')):
+        name = player.find('name').text if player.find('name') is not None else None
+        location = player.find('location').text if player.find('location') is not None else None
+        birthdate = player.find('birthdate').text if player.find('birthdate') is not None else None
+        rating = player.find('rating').text if player.find('rating') is not None else None
+
+        # Проверяем, есть ли необходимая информация перед добавлением в список
+        if name and location and birthdate and rating:
+            player_info = {
+                '№': idx + 1,  # Индексация начинается с 1
+                'ФИО': name,
+                'Город': location,
+                'Год рождения': birthdate,
+                'рейтинг': rating
+            }
+            players_data.append(player_info)
+    players_df = pd.DataFrame(players_data)
+    
+    # Парсим информацию о турнире
+    tournament_data = []
+    tournament = root.find('.//Tournament')
+    if tournament is not None:
+        header = tournament.find('Header')
+        if header is not None:
+            tournament_info = {
+                'Дата': header.find('date').text if header.find('date') is not None else '',
+                'Название': header.find('name').text if header.find('name') is not None else '',
+                'Адрес': header.find('addr').text if header.find('addr') is not None else '',
+                'Организатор': header.find('organizer').text if header.find('organizer') is not None else '',
+                'Количество игроков': header.find('numPlayers').text if header.find('numPlayers') is not None else '',
+                'Колличество столов': header.find('numTables').text if header.find('numTables') is not None else ''
+            }
+            tournament_data.append(tournament_info)
+
+    # Преобразуем данные о турнире в формат DataFrame
+    tournament_df = pd.DataFrame(tournament_data)
+    
+    return players_df, tournament_df
+
+def parse_xml_and_save_to_excel(xml_file_path, excel_file_path):
+    # Загружаем и разбираем XML файл
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
+    
+    # Парсим информацию о игроках
+    players_data = []
+    for idx, player in enumerate(root.findall('.//Player')):
+        name = player.find('name').text if player.find('name') is not None else None
+        location = player.find('location').text if player.find('location') is not None else None
+        birthdate = player.find('birthdate').text if player.find('birthdate') is not None else None
+        rating = player.find('rating').text if player.find('rating') is not None else None
+
+        # Проверяем, есть ли необходимая информация перед добавлением в список
+        if name and location and birthdate and rating:
+            player_info = {
+                '№': idx + 1,  # Индексация начинается с 1
+                'ФИО': name,
+                'Город': location,
+                'Год рождения': birthdate,
+                'рейтинг': rating
+            }
+            players_data.append(player_info)
+    players_df = pd.DataFrame(players_data)
+    
+    # Парсим информацию о турнире
+    tournament_data = []
+    tournament = root.find('.//Tournament')
+    if tournament is not None:
+        header = tournament.find('Header')
+        if header is not None:
+            tournament_info = {
+                'Дата': header.find('date').text if header.find('date') is not None else '',
+                'Название': header.find('name').text if header.find('name') is not None else '',
+                'Адрес': header.find('addr').text if header.find('addr') is not None else '',
+                'Организатор': header.find('organizer').text if header.find('organizer') is not None else '',
+                'Количество игроков': header.find('numPlayers').text if header.find('numPlayers') is not None else '',
+                'Колличество столов': header.find('numTables').text if header.find('numTables') is not None else ''
+            }
+            tournament_data.append(tournament_info)
+
+    # Преобразуем данные о турнире в формат DataFrame
+    tournament_df = pd.DataFrame(tournament_data)
+    
+    # Сохраняем в Excel с заданной шириной столбцов и выравниванием
+    with pd.ExcelWriter(excel_file_path, engine='xlsxwriter') as writer:
+        players_df.to_excel(writer, sheet_name='Players', index=False)
+        tournament_df_formatted = pd.DataFrame(tournament_df.stack().reset_index())
+        tournament_df_formatted = tournament_df_formatted.iloc[:, 1:]  # Игнорируем столбец с индексом
+        tournament_df_formatted.to_excel(writer, sheet_name='Tournament', index=False, header=False)
+
+        workbook = writer.book
+        center_format = workbook.add_format({'align': 'center'})
+
+        # Получаем объект worksheet, чтобы установить настройки форматирования
+        worksheet_players = writer.sheets['Players']
+        worksheet_players.set_column('A:A', 4)  # number
+        worksheet_players.set_column('B:B', 35)  # name, 7.5cm approx 28
+        worksheet_players.set_column('C:C', 17, center_format)  # location, 3.5cm approx 13
+        worksheet_players.set_column('D:D', 14, center_format)  # birthdate, 3.2cm approx 12
+        worksheet_players.set_column('E:E', 12, center_format)  # rating
+
+        worksheet_tournament = writer.sheets['Tournament']
+        worksheet_tournament.set_column('A:A', 17)
+        worksheet_tournament.set_column('B:B', 38)
+
+    return players_df, tournament_df
+
